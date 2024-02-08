3f02b7d41fc733181d97bad4875ab027c3f0e1d2
nrmnqdds <quddusnuriman@gmail.com>
2024-02-08 10:30:08 +0800

    :zap: blazingly fast scraper

--- src/app/api/timetables/import/imaluum/getSchedulesFromContent.ts
+++ src/app/api/timetables/import/imaluum/getSchedulesFromContent.ts
@@ -1,42 +1,91 @@
-import * as cheerio from "cheerio";
 import capitalize from "@/utils/common/strings/capitalize";
+import * as cheerio from "cheerio";
+import { parse } from "node-html-parser";
 import getTimestamps from "./getTimestamps";
 
 export default function getSchedulesFromContent(content: string): Schedule[] {
-  const $ = cheerio.load(content);
-  let schedules: Schedule[] = [];
+	// console.time("getSchedulesFromContent");
+	const $ = cheerio.load(content);
+	const schedules: Schedule[] = [];
 
-  // Get and iterate the table rows
-  const rows = $("table > tbody > tr");
-  rows.each((i, row) => {
-    const cells = $(row).find("td");
+	// Get and iterate the table rows
+	const rows = $("table > tbody > tr");
+	rows.each((i, row) => {
+		const cells = $(row).find("td");
 
-    const code = $(cells[0]).text();
-    const title = $(cells[1]).text();
-    const sect = $(cells[2]).text();
-    const chr = $(cells[3]).text();
-    const status = $(cells[4]).text();
-    const day = $(cells[5]).text();
-    const time = $(cells[6]).text();
-    const venue = $(cells[7]).text();
-    const lecturer = $(cells[8]).text();
+		const code = $(cells[0]).text();
+		const title = $(cells[1]).text();
+		const sect = $(cells[2]).text();
+		const chr = $(cells[3]).text();
+		const status = $(cells[4]).text();
+		const day = $(cells[5]).text();
+		const time = $(cells[6]).text();
+		const venue = $(cells[7]).text();
+		const lecturer = $(cells[8]).text();
 
-    // If the status is not "Registered", skip
-    if (status !== "Registered") return;
+		// If the status is not "Registered", skip
+		if (status !== "Registered") return;
 
-    // Check if day and time are not empty
-    if (day === "" || time === "") return;
+		// Check if day and time are not empty
+		if (day === "" || time === "") return;
 
-    schedules.push({
-      code: code,
-      title: capitalize(title),
-      section: parseInt(sect),
-      creditHours: parseFloat(chr),
-      lecturer: capitalize(lecturer),
-      venue: venue,
-      weekTimes: getTimestamps(day, time),
-    });
-  });
+		schedules.push({
+			code: code,
+			title: capitalize(title),
+			section: parseInt(sect),
+			creditHours: parseFloat(chr),
+			lecturer: capitalize(lecturer),
+			venue: venue,
+			weekTimes: getTimestamps(day, time),
+		});
+	});
 
-  return schedules;
+	// console.timeEnd("getSchedulesFromContent");
+
+	return schedules;
+}
+
+export function getSchedulesFromContentNew(content: string) {
+	// console.time("getSchedulesFromContentNew");
+	const root = parse(content);
+
+	const table = root.querySelector(".box-body table.table.table-hover");
+	const rows = table?.querySelectorAll("tr") ?? [];
+
+	const schedules: Schedule[] = [];
+
+	// @ts-ignore
+	for (const row of rows) {
+		const tds = row.querySelectorAll("td");
+		if (tds.length === 0) continue;
+
+		if (tds.length >= 9) {
+			const code = tds[0].textContent.trim();
+			const title = tds[1].textContent.trim();
+			const sect = tds[2].textContent.trim();
+			const chr = tds[3].textContent.trim();
+			const status = tds[4].textContent.trim();
+			if (status !== "Registered") continue;
+			const day = tds[5].textContent.trim();
+			if (day === "" || day === null) continue;
+			const time = tds[6].textContent.trim();
+			if (time === "" || time === null) continue;
+			const venue = tds[7].textContent.trim();
+			const lecturer = tds[8].textContent.trim();
+
+			schedules.push({
+				code: code,
+				title: capitalize(title),
+				section: parseInt(sect),
+				creditHours: parseFloat(chr),
+				lecturer: capitalize(lecturer),
+				venue: venue,
+				weekTimes: getTimestamps(day, time),
+			});
+		}
+	}
+
+	// console.timeEnd("getSchedulesFromContentNew");
+
+	return schedules;
 }
--- src/app/api/timetables/import/imaluum/getSessions.ts
+++ src/app/api/timetables/import/imaluum/getSessions.ts
@@ -1,33 +1,74 @@
 import * as cheerio from "cheerio";
+import { parse } from "node-html-parser";
 
 type Session = {
-  year: string;
-  semester: number;
+	year: string;
+	semester: number;
 };
 
 export default function getSessions(content: string): {
-  currentSession: Session;
-  sessions: Session[];
+	currentSession: Session;
+	sessions: Session[];
 } {
-  const $ = cheerio.load(content);
+	// console.time("getSessions");
+	const $ = cheerio.load(content);
 
-  const temp = $("h3.box-title").text().trim().replace(",", "").split(" ");
-  const currentSession: Session = {
-    year: temp.at(-1) || "",
-    semester: parseInt(temp.at(-2) || "0"),
-  };
-  const sessions: Session[] = $("ul.dropdown-menu > li > a")
-    .map((i, el) => ({
-      // Original string: Sem X, 20XX/20XX
+	const temp = $("h3.box-title").text().trim().replace(",", "").split(" ");
+	const currentSession: Session = {
+		year: temp.at(-1) || "",
+		semester: parseInt(temp.at(-2) || "0"),
+	};
+	const sessions: Session[] = $("ul.dropdown-menu > li > a")
+		.map((i, el) => ({
+			// Original string: Sem X, 20XX/20XX
 
-      // Get session: 20XX/20XX
-      year: $(el).text().split(", ")[1],
+			// Get session: 20XX/20XX
+			year: $(el).text().split(", ")[1],
 
-      // Get semester: X
-      semester: parseInt($(el).text().split(", ")[0].split(" ")[1]),
-    }))
-    .get()
-    .reverse();
+			// Get semester: X
+			semester: parseInt($(el).text().split(", ")[0].split(" ")[1]),
+		}))
+		.get()
+		.reverse();
 
-  return { currentSession, sessions };
+	// console.timeEnd("getSessions");
+
+	return { currentSession, sessions };
+}
+
+// optimized version
+export function getSessionsNew(content: string) {
+	// console.time("getSessionsNew");
+
+	const root = parse(content);
+
+	const sessionBody = root.querySelectorAll(
+		".box.box-primary .box-header.with-border .dropdown ul.dropdown-menu li[style*='font-size:16px']",
+	);
+
+	const sessionList = sessionBody.map((element) => {
+		const row = element;
+		const sessionName = row.querySelector("a")?.textContent.trim();
+		return sessionName;
+	});
+
+	if (sessionList.length === 0) {
+		throw new Error("Session list not found");
+	}
+
+	const currentSession: Session = {
+		year: sessionList.at(-1).split(", ")[1],
+		semester: parseInt(sessionList.at(-1).split(", ")[0].split(" ")[1]),
+	};
+
+	const sessions: Session[] = sessionList.map((session) => {
+		return {
+			year: session.split(", ")[1],
+			semester: parseInt(session.split(", ")[0].split(" ")[1]),
+		};
+	});
+
+	// console.timeEnd("getSessionsNew");
+
+	return { currentSession, sessions: sessions.reverse() };
 }
